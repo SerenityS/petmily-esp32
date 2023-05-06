6b5366fa2c81fbe7df925eaeffa0bfa7a62def47
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2023-05-07 05:11:30 +0900

    feat: add WifiService
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

---
+++ WifiService.ino
@@ -0,0 +1,57 @@
+// Wifi
+#include <WiFi.h>
+
+// Preference
+#include <Preferences.h>
+
+Preferences pref;
+
+// Wifi Credential
+String ssid;
+String pw;
+
+// Status Variable
+bool hasCredentials = false;
+volatile bool isConnected = false;
+bool connStatusChanged = false;
+
+bool checkCred() {
+  pref.begin("WifiCred", false);
+  bool hasCred = pref.getBool("valid", false);
+  if (hasCred) {
+    ssid = pref.getString("ssid", "");
+    pw = pref.getString("pw", "");
+
+    if (ssid.equals("")
+        || pw.equals("")) {
+      Serial.println("Found preferences but credentials are invalid");
+    } else {
+      Serial.println("Read from preferences:");
+      Serial.println("SSID: " + ssid + " password: " + pw);
+      hasCredentials = true;
+    }
+  } else {
+    Serial.println("Could not find preferences, need send data over BLE");
+  }
+  pref.end();
+  return hasCredentials;
+}
+
+void connectWifi(String ssid, String pw) {
+  WiFi.onEvent(WiFiEvent);
+  WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
+  WiFiEventId_t eventID = WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info) {
+    Serial.print("WiFi lost connection. Reason: ");
+    Serial.println(info.wifi_sta_disconnected.reason);
+  }, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
+
+  WiFi.disconnect(true);
+  WiFi.enableSTA(true);
+  WiFi.mode(WIFI_STA);
+
+  Serial.print("Start Wifi Connection to ");
+  Serial.print(ssid);
+  Serial.println(pw);
+
+  WiFi.begin(ssid.c_str(), pw.c_str());
+}
