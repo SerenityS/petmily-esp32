165609c4c32d86710a608e6082bc66eb20ad5313
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-06-19 00:17:25 +0900

    feat: support 2 scale
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- HX711.ino
+++ HX711.ino
@@ -25,25 +25,40 @@
 
 #include "HX711.h"  //This library can be obtained here http://librarymanager/All#Avia_HX711
 
-#define calibration_factor -7050.0  //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define bowl_calibration_factor -23950.0      //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define feed_box_calibration_factor -23950.0  //This value is obtained using the SparkFun_HX711_Calibration sketch
 
-#define LOADCELL_DOUT_PIN 23
-#define LOADCELL_SCK_PIN 22
 
-extern HX711 scale;
+#define BOWL_DOUT_PIN 4
+#define BOWL_SCK_PIN 2
+
+#define FEED_BOX_DOUT_PIN 23
+#define FEED_BOX_SCK_PIN 22
+
+extern HX711 bowl_scale;
+extern HX711 feed_box_scale;
 extern bool is_scale_enable = true;
 
 void HX711_setup() {
   Serial.println("Initialize HX711...");
 
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(calibration_factor);  //This value is obtained by using the SparkFun_HX711_Calibration sketch
-  scale.tare();                         //Assuming there is no weight on the scale at start up, reset the scale to 0
+  bowl_scale.begin(BOWL_DOUT_PIN, BOWL_SCK_PIN);
+  bowl_scale.set_scale(bowl_calibration_factor);  //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  bowl_scale.tare();                 //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  feed_box_scale.begin(FEED_BOX_DOUT_PIN, FEED_BOX_SCK_PIN);
+  feed_box_scale.set_scale(feed_box_calibration_factor);  //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  feed_box_scale.set_offset(-602988);                            //Assuming there is no weight on the scale at start up, reset the scale to 0
 }
 
 void HX711_print() {
-  Serial.print("HX711 Reading: ");
-  Serial.print(scale.get_units(), 1);  //scale.get_units() returns a float
-  Serial.print("Kg");                  //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.print("Bowl Reading: ");
+  Serial.print(bowl_scale.get_units(3), 3);  //scale.get_units() returns a float
+  Serial.print("g");                         //You can change this to kg but you'll need to refactor the calibration_factor
+  Serial.println();
+
+  Serial.print("Feed Box Reading: ");
+  Serial.print(feed_box_scale.get_units(), 1);  //scale.get_units() returns a float
+  Serial.print("Kg");                           //You can change this to kg but you'll need to refactor the calibration_factor
   Serial.println();
 }
