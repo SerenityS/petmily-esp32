92a2254af0f3078eec47b01dd1b616623359916a
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-06-19 00:19:32 +0900

    feat: add postHistory
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- HttpREST.ino
+++ HttpREST.ino
@@ -4,9 +4,11 @@
 #include <time.h>
 
 String deviceDataUrl = "http://ys.qwertycvb.com:10115/petmily/device_data";
+String historyUrl = "http://ys.qwertycvb.com:10115/history";
 String scheduleUrl = "http://ys.qwertycvb.com:10115/schedule";
 
-extern HX711 scale;
+extern HX711 bowl_scale;
+extern HX711 feed_box_scale;
 
 void postDeviceData() {
   HTTPClient http;
@@ -14,7 +16,7 @@ void postDeviceData() {
   http.begin(deviceDataUrl);
   http.addHeader("Content-Type", "application/json");
 
-  String deviceDataJson = "{\"chip_id\": \"" + String(ESP.getEfuseMac(), HEX) + "\", \"bowl_amount\": " + scale.get_units() + ", \"feed_box_amount\": 1000}";
+  String deviceDataJson = "{\"chip_id\": \"" + String(ESP.getEfuseMac(), HEX) + "\", \"bowl_amount\": " + bowl_scale.get_units(3) * 1000 + ", \"feed_box_amount\":" + feed_box_scale.get_units(3) * 1000 + "}";
 
   int httpResponseCode = http.POST(deviceDataJson);
 
@@ -22,6 +24,30 @@ void postDeviceData() {
     String response = http.getString();
 
     Serial.print("postDeviceData: ");
+    Serial.println(httpResponseCode);
+    Serial.println(response);
+  } else {
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
+
+void postHistory(int consume) {
+  HTTPClient http;
+
+  http.begin(historyUrl);
+  http.addHeader("Content-Type", "application/json");
+
+  String historyJson = "{\"user_id\": \"069cda42-2c51-414d-ba22-a7e4ec49bff8\", \"chip_id\": \"" + String(ESP.getEfuseMac(), HEX) + "\", \"consume\": " + consume + "}";
+
+  int httpResponseCode = http.POST(historyJson);
+
+  if (httpResponseCode > 0) {
+    String response = http.getString();
+
+    Serial.print("postHistory: ");
     Serial.println(httpResponseCode);
     Serial.println(response);
   } else {
