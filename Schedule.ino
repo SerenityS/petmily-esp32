4a26de348408091e97d5ccc7773637f0a47c8248
qwertycvb(HeeRyun, Jin <jins4218@gmail.com>
2023-06-19 00:17:45 +0900

    feat: implement feeding
    Signed-off-by: qwertycvb(HeeRyun, Jin <jins4218@gmail.com>

--- Schedule.ino
+++ Schedule.ino
@@ -33,9 +33,11 @@ void Schedule_check() {
     strptime(date.c_str(), "%Y-%m-%dT%H:%M:%S.%03d", &scheduleTime);
 
     if (isEnable && localTime.tm_hour == localTime.tm_hour && localTime.tm_min == scheduleTime.tm_min) {
-      Serial.print("Run Feeding, amount:");
+      Serial.print("Schedule: Run Feeding, amount:");
       Serial.println(amount);
-      // TODO: Implement Feeding
+      Servo_open();
+      delay(1500);
+      Servo_close();
     }
   }
 }
